51d43bfc263febd967f135e10c8b2641aa51ad5a
TANG Ho Tung,Randy <hotung1027@gmail.com>
2024-02-22 01:41:01 +0800

    WIP : Lesson 4

--- Lesson 4 - Tracking Image Features/Harris Corner Detection/cornerness_harris/src/cornerness_harris.cpp
+++ Lesson 4 - Tracking Image Features/Harris Corner Detection/cornerness_harris/src/cornerness_harris.cpp
@@ -1,46 +1,100 @@
 #include <iostream>
 #include <numeric>
 #include <opencv2/core.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/features2d.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/features2d.hpp>
+#include <vector>
 
 using namespace std;
 
-void cornernessHarris()
-{
-    // load image from file
-    cv::Mat img;
-    img = cv::imread("../images/img1.png");
-    cv::cvtColor(img, img, cv::COLOR_BGR2GRAY); // convert to grayscale
+// Non Max Suppression
+void nms(cv::Mat &src, int minResponse, vector<cv::KeyPoint> &keypoints,
+         int radius) {
 
-    // Detector parameters
-    int blockSize = 2;     // for every pixel, a blockSize × blockSize neighborhood is considered
-    int apertureSize = 3;  // aperture parameter for Sobel operator (must be odd)
-    int minResponse = 100; // minimum value for a corner in the 8bit scaled response matrix
-    double k = 0.04;       // Harris parameter (see equation for details)
+  double maxOverlap = 0.0;
+  for (size_t j = 0; j < src.rows; j++) {
+    for (size_t i = 0; i < src.cols; i++) {
 
-    // Detect Harris corners and normalize output
-    cv::Mat dst, dst_norm, dst_norm_scaled;
-    dst = cv::Mat::zeros(img.size(), CV_32FC1);
-    cv::cornerHarris(img, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
-    cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
-    cv::convertScaleAbs(dst_norm, dst_norm_scaled);
+      int response = (int)src.at<float>(j, i);
 
-    // visualize results
-    string windowName = "Harris Corner Detector Response Matrix";
-    cv::namedWindow(windowName, 4);
-    cv::imshow(windowName, dst_norm_scaled);
-    cv::waitKey(0);
+      if (response > minResponse) { // only store points above the threshold
+        cv::KeyPoint newKeyPoint;
+        newKeyPoint.pt = cv::Point2f(i, j);
+        newKeyPoint.size = 2 * radius;
+        newKeyPoint.response = response;
 
-    // TODO: Your task is to locate local maxima in the Harris response matrix 
-    // and perform a non-maximum suppression (NMS) in a local neighborhood around 
-    // each maximum. The resulting coordinates shall be stored in a list of keypoints 
-    // of the type `vector<cv::KeyPoint>`.
-
+        // Perform non-max suppression over local neighbors
+        bool detectNonMax = false;
+        for (auto pt = keypoints.begin(); pt != keypoints.end();
+             ++pt) { // iterate over existing keypoints
+          double overlap = cv::KeyPoint::overlap(
+              newKeyPoint, *pt); // check if new keypoints match existing
+                                 // keypoints by overlapping local regions
+          if (overlap > maxOverlap) {
+            detectNonMax = true; // found overlapping keypoints
+            if (newKeyPoint.response >
+                (*pt).response) {  // walk through existing
+                                   // keypoints local maximum
+              (*pt) = newKeyPoint; // replace existing keypoint with new one
+              break;
+            }
+          }
+        }
+        if (!detectNonMax) // only store new keypoints if no overlap has
+                           // been found
+        {
+          keypoints.push_back(newKeyPoint); // store the new keypoint
+        }
+      }
+    }
+  }
 }
 
-int main()
-{
-    cornernessHarris();
+void cornernessHarris() {
+  // load image from file
+  cv::Mat img;
+  img = cv::imread("../images/img1.png");
+  cv::cvtColor(img, img, cv::COLOR_BGR2GRAY); // convert to grayscale
+
+  // Detector parameters
+  int blockSize =
+      2; // for every pixel, a blockSize × blockSize neighborhood is considered
+  int apertureSize = 3; // aperture parameter for Sobel operator (must be odd)
+  int minResponse =
+      100; // minimum value for a corner in the 8bit scaled response matrix
+  double k = 0.04; // Harris parameter (see equation for details)
+
+  // Detect Harris corners and normalize output
+  cv::Mat dst, dst_norm, dst_norm_scaled;
+  dst = cv::Mat::zeros(img.size(), CV_32FC1);
+  cv::cornerHarris(img, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
+  cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+  cv::convertScaleAbs(dst_norm, dst_norm_scaled);
+
+  // visualize results
+  string windowName = "Harris Corner Detector Response Matrix";
+  cv::namedWindow(windowName, 4);
+  cv::imshow(windowName, dst_norm_scaled);
+  cv::waitKey(0);
+
+  // TODO: Your task is to locate local maxima in the Harris response matrix
+  // and perform a non-maximum suppression (NMS) in a local neighborhood around
+  // each maximum. The resulting coordinates shall be stored in a list of
+  // keypoints of the type `vector<cv::KeyPoint>`.
+  vector<cv::KeyPoint> keypoints;
+
+  // Non-maximum suppression window
+  nms(dst_norm, minResponse, keypoints, apertureSize);
+
+  windowName = "Harris Corner Detection Results";
+  cv::namedWindow(windowName, 5);
+  cv::Mat visImage = dst_norm_scaled.clone();
+  cv::drawKeypoints(dst_norm_scaled, keypoints, visImage, cv::Scalar::all(-1),
+                    cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+  cv::imshow(windowName, visImage);
+  cv::waitKey(0);
 }
+
+int main() { cornernessHarris(); }
--- Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/magnitude_sobel.cpp
+++ Lesson 4 - Tracking Image Features/Intensity Gradient and Filtering/gradient_filtering/src/magnitude_sobel.cpp
@@ -1,43 +1,89 @@
 #include <iostream>
 #include <numeric>
 #include <opencv2/core.hpp>
+#include <opencv2/core/base.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
 using namespace std;
 
-void magnitudeSobel()
-{
-    // load image from file
-    cv::Mat img;
-    img = cv::imread("./images/img1gray.png");
+void magnitudeSobel() {
+  // load image from file
+  cv::Mat img;
+  img = cv::imread("../images/img1gray.png");
 
-    // convert image to grayscale
-    cv::Mat imgGray;
-    cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
+  float sobel_x[9] = {-1, 0, +1, -2, 0, +2, -1, 0, +1};
+  float sobel_y[9] = {-1, +2, +1, 0, 0, 0, -1, +2, +1};
 
-    // apply smoothing using the GaussianBlur() function from the OpenCV
-    // ToDo : Add your code here
+  // convert image to grayscale
+  cv::Mat imgGray;
+  cv::Mat imgBlur;
+  cv::Mat magnitude;
+  cv::Mat sobel_x_mat;
+  cv::Mat sobel_y_mat;
+  cv::Mat sobel_mag;
+  cv::Mat sobel_ang;
+  cv::cvtColor(img, imgGray, cv::COLOR_BGR2GRAY);
 
-    // create filter kernels using the cv::Mat datatype both for x and y
-    // ToDo : Add your code here
+  // apply smoothing using the GaussianBlur() function from the OpenCV
+  // ToDo : Add your code here
+  cv::Size blur_size(5, 5);
+  cv::GaussianBlur(imgGray, imgBlur, blur_size, 1);
 
-    // apply filter using the OpenCv function filter2D()
-    // ToDo : Add your code here
-    
+  // create filter kernels using the cv::Mat datatype both for x and y
+  // ToDo : Add your code here
+  cv::Mat sobel_x_kernel = cv::Mat(3, 3, CV_32F, sobel_x);
+  cv::Mat sobel_y_kernel = cv::Mat(3, 3, CV_32F, sobel_y);
 
-    // compute magnitude image based on the equation presented in the lesson 
-    // ToDo : Add your code here
-    
+  // apply filter using the OpenCv function filter2D()
+  // ToDo : Add your code here
+  cv::filter2D(imgBlur, sobel_x_mat, -1, sobel_x_kernel, cv::Point(-1, -1), 0,
+               cv::BORDER_DEFAULT);
 
-    // show result
-    string windowName = "Gaussian Blurring";
-    cv::namedWindow(windowName, 1); // create window
-    cv::imshow(windowName, magnitude);
-    cv::waitKey(0); // wait for keyboard input before continuing
+  cv::filter2D(imgBlur, sobel_y_mat, -1, sobel_y_kernel, cv::Point(-1, -1), 0,
+               cv::BORDER_DEFAULT);
+
+  // compute magnitude image based on the equation presented in the lesson
+  // ToDo : Add your code here
+  cv::convertScaleAbs(sobel_x_mat, sobel_x_mat);
+  cv::convertScaleAbs(sobel_y_mat, sobel_y_mat);
+
+  cv::addWeighted(sobel_x_mat, 0.5, sobel_y_mat, 0.5, 0, magnitude);
+  string windowName = "Gaussian Blurring";
+  cv::namedWindow(windowName, 1); // create window
+  cv::imshow(windowName, magnitude);
+
+  sobel_y_mat.convertTo(sobel_y_mat, CV_32F);
+  sobel_x_mat.convertTo(sobel_x_mat, CV_32F);
+
+  cv::phase(sobel_x_mat, sobel_y_mat, sobel_ang);
+
+  cv::pow(sobel_x_mat, 2, sobel_x_mat);
+  cv::pow(sobel_y_mat, 2, sobel_y_mat);
+  cv::sqrt(sobel_x_mat + sobel_y_mat, sobel_mag);
+
+  sobel_mag.convertTo(sobel_mag, CV_8U);
+  sobel_ang.convertTo(sobel_ang, CV_8U);
+
+  cv::multiply(sobel_mag, sobel_ang, magnitude, 0.5);
+
+  // cv::cvtColor(sobel_mag, sobel_mag, cv::COLOR_GRAY2BGR);
+  // cv::cvtColor(sobel_ang, sobel_ang, cv::COLOR_GRAY2BGR);
+
+  string sobel_mag_win = "Sobel Magnitude";
+  cv::namedWindow(sobel_mag_win, 1); // create window
+  cv::imshow(sobel_mag_win, sobel_mag * 0.5);
+
+  string sobel_ang_win = "Sobel Angle";
+  cv::namedWindow(sobel_ang_win, 1); // create window
+  cv::imshow(sobel_ang_win, sobel_ang);
+
+  string sobel = "Sobel";
+  cv::namedWindow(sobel, 1); // create window
+  cv::imshow(sobel, magnitude);
+
+  // show result
+  cv::waitKey(0); // wait for keyboard input before continuing
 }
 
-int main()
-{
-    magnitudeSobel();
-}
+int main() { magnitudeSobel(); }
